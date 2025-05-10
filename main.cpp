aec7e287378a8c2b5146f55e7fe1a429c429dc44
anirban <anirban12321@gmail.com>
2025-05-11 03:12:57 +0530

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,135 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include <iostream>
 
+const int WIDTH = 1024;
+const int HEIGHT = 768;
+
+const char *vertexShaderSource = R"(
+#version 330 core
+layout (location = 0) in vec2 aPos;
+out vec2 TexCoord;
+void main() {
+    TexCoord = aPos * 0.5 + 0.5;
+    gl_Position = vec4(aPos, 0.0, 1.0);
+}
+)";
+
+const char *fragmentShaderSource = R"(
+#version 330 core
+in vec2 TexCoord;
+out vec4 FragColor;
+
+uniform sampler2D u_image;
+
+void main() {
+    FragColor = texture(u_image, TexCoord);
+}
+)";
+
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  if (!glfwInit()) {
+    std::cerr << "Failed to init GLFW\n";
+    return -1;
+  }
+
+  // Setup OpenGL context version
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on macOS
+#endif
+
+  GLFWwindow *window =
+      glfwCreateWindow(WIDTH, HEIGHT, "Pixel Shader Grid", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "Failed to create GLFW window\n";
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (glewInit() != GLEW_OK) {
+    std::cerr << "Failed to init GLEW\n";
+    return -1;
+  }
+
+  // Compile vertex shader
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+  glCompileShader(vertexShader);
+
+  // Compile fragment shader
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+  glCompileShader(fragmentShader);
+
+  // Link program
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
+
+  // Fullscreen quad
+  float quadVertices[] = {
+      -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+  };
+
+  GLuint vao, vbo;
+  glGenVertexArrays(1, &vao);
+  glGenBuffers(1, &vbo);
+
+  glBindVertexArray(vao);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices,
+               GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+  glEnableVertexAttribArray(0);
+
+  unsigned char pixels[WIDTH * HEIGHT * 3];
+
+  for (int y = 0; y < HEIGHT; ++y) {
+    for (int x = 0; x < WIDTH; ++x) {
+      int index = (y * WIDTH + x) * 3;
+
+      // Scale x and y to 0–255 range
+      uint8_t r = (x / (float)(WIDTH)) * 255.0f;
+      uint8_t g = (y / (float)(HEIGHT)) * 255.0f;
+
+      pixels[index + 0] = r; // Red: 0-255 based on x
+      pixels[index + 1] = g; // Green: 0-255 based on y
+      pixels[index + 2] = 0; // Blue: 0
+    }
+  }
+
+  GLuint textureID;
+  glGenTextures(1, &textureID);
+  glBindTexture(GL_TEXTURE_2D, textureID);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB,
+               GL_UNSIGNED_BYTE, pixels);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glActiveTexture(GL_TEXTURE0); // Use texture unit 0
+    glBindTexture(GL_TEXTURE_2D,
+                  textureID); // textureID is your uploaded texture
+
+    GLint location = glGetUniformLocation(shaderProgram, "u_image");
+    glUniform1i(location, 0);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
